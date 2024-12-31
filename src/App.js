b005fc4be0107f221b22092471bb6034413a6921
DavidEValdes <36570117+DavidEValdes@users.noreply.github.com>
2024-12-30 19:22:27 -0500

    Clean up default CRA files and fix main component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
+import './App.css';
 import HealthWebsite from './HealthWebsite';
 
 function App() {
-  return (
-    <div className="App">
-      <HealthWebsite />
-    </div>
-  );
+  return <HealthWebsite />;
 }
 
 export default App;
